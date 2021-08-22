883867019d18a215e3dfe87deb9c861bd3221022
iluxandro01 <68388374+iluxandro01@users.noreply.github.com>
2021-08-22 22:56:38 +0300

    Part 4

---
+++ Assets/Pauser.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+public class Pauser : MonoBehaviour
+{
+    private bool _isPaused;
+
+    public Action<bool> ONStatePauseChanged;
+    
+    public void ChangeState()
+    {
+        if (_isPaused)
+        {
+            UnPause();
+        }
+        else
+        {
+            Pause();
+        }
+        
+        ONStatePauseChanged?.Invoke(_isPaused);
+    }
+    
+    private void Pause()
+    {
+        _isPaused = true;
+        Time.timeScale = 0f;
+    }
+
+    private void UnPause()
+    {
+        _isPaused = false;
+        Time.timeScale = 1f;
+    }
+}
--- Assets/Scripts/ClickZone.cs
+++ Assets/Scripts/ClickZone.cs
@@ -4,10 +4,26 @@ using UnityEngine.EventSystems;
 
 public class ClickZone : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField] private Pauser _pauser;
     public event Action ONClicked;
-    
+
+    private void Start()
+    {
+        _pauser.ONStatePauseChanged += SetActive;
+    }
+
+    private void SetActive(bool isActive)
+    {
+        gameObject.SetActive(!isActive);
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         ONClicked?.Invoke();
+    }
+
+    private void OnDestroy()
+    {
+        _pauser.ONStatePauseChanged -= SetActive;
     }
 }
